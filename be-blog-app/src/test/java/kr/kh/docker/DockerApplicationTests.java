8cf81cd1bf8e5730d8af0ee3e21316ab3cf6c455
KyungHO123 <152416402+KyungHO123@users.noreply.github.com>
2025-03-27 14:31:43 +0900

    첫 커밋

---
+++ be-blog-app/src/test/java/kr/kh/docker/DockerApplicationTests.java
@@ -0,0 +1,13 @@
+package kr.kh.docker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DockerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
